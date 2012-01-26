8f81baff666b30700ba732964b5d2705ae754e95
Steven Klise <steven@stevenklise.com>
2012-01-26 14:46:15 -0500

    Views, REST

--- application.rb
+++ application.rb
@@ -1,20 +1,23 @@
 require 'bundler'
 Bundler.require
 
-DataMapper.setup(:default, ENV['DATABASE_URL'] || 'sqlite://development.sqlite'
+DataMapper.setup(:default, ENV['DATABASE_URL'] || "sqlite://#{Dir.pwd}/development.sqlite")
+
+class Thing
+  include DataMapper::Resource
+  
+  property :id, Serial, :key => true
+  property :title, String
+  property :description, Text
+end
 
 get '/' do
   erb :index
 end
 
-get '/things/' do
-  @things = Things.all
+get '/things' do
+  @things = Thing.all
   erb :things_index
-end
-
-get '/things/:id' do
-  @thing = Thing.get(params[:id])
-  erb :things_show
 end
 
 get '/things/new' do
@@ -22,9 +25,14 @@ get '/things/new' do
 end
 
 post '/things' do
-  @thing = Thing.new(params)
+  @thing = Thing.create(params)
   @thing.save
   redirect '/things'
+end
+
+get '/things/:id' do
+  @thing = Thing.get(params[:id])
+  erb :things_show
 end
 
 get '/things/:id/edit' do
--- config.ru
+++ config.ru
@@ -1,8 +1,8 @@
 # This is the file that Heroku uses to launch Sinatra applications. So this
 # file needs to know about all the code from the other files that make up
-# the applicaton. Since we made `index.rb` reference all other files,
+# the applicaton. Since we made `application.rb` reference all other files,
 # loading that file will bring the rest of the code.
-require './index'
+require './application'
 # Note the "./" before the file name are required for Ruby 1.9.X.
 # Also, when loading .rb files the extension can be omitted.
 
